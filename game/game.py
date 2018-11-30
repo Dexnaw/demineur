6ba488f7a9f61bdc61b68cda80c502cb7c4dc150
Dexnaw <arnaud.urbain@realdolmen.com>
2018-11-30 15:53:30 +0100

    :construction: first window

--- game/game.py
+++ game/game.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from tkinter import *
+
+#racine de la fenetre
+fenetre = Tk()
+champ_label = Label(fenetre, text="Hello World!")
+
+champ_label.pack()
+
+fenetre.mainloop()
